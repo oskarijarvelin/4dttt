f5ffdefed84c7ddc6dbb4ea897e9f476b5b4d245
SurrealPartisan <SurrealPartisan>
2016-12-27 17:04:19 +0200

    Add squares to table.

--- main.py
+++ main.py
@@ -26,11 +26,17 @@ class TTT_Table:
 	def __init__(self, base, power, players):
 		self.base = base
 		self.power = power
+		self.squares = {}
+		self.add_squares_recursive(self.power - 1, [])
 		self.players = players
 		self.turn = self.players[0]
 		
-	def add_squares_recursive(self):
-		pass
+	def add_squares_recursive(self, powerleft, list):
+		for i in range(0, self.power):
+			if powerleft == 0:
+				self.squares[''.join(list + [str(i)])] = Square(list + [i])
+			else:
+				self.add_squares_recursive(powerleft - 1, list + [str(i)])
 		
 	def list_lines(self):
 		pass
@@ -43,7 +49,9 @@ class TTT_Table:
 	
 class Player:
 	
-	def __init__(self, name, mark)
+	def __init__(self, name, mark):
 		self.name = name
 		self.mark = mark
 	
+table = TTT_Table(4, 4, [1])
+print(table.squares)
