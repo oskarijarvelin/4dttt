97c7dcfe7077a7d9f1ec0033a927b7a56ceda30a
SurrealPartisan <SurrealPartisan>
2016-12-27 16:09:49 +0200

    General structure.

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-print("Hello World!")
+class Square:
+	
+	def __init__(self, coords):
+		pass
+		
+	def add_mark(self, player):
+		pass
+	
+class Line:
+	
+	def __init__(self, params):
+		pass
+		
+	def squares(self):
+		pass
+	
+	def win(self):
+		pass
+		
+	def not_winnable(self):
+		pass
+	
+
+class TTT_Table:
+	
+	def __init__(self, base, power, players):
+		self.base = base
+		self.power = power
+		self.players = players
+		self.turn = self.players[0]
+		
+	def add_squares_recursive(self):
+		pass
+		
+	def list_lines(self):
+		pass
+	
+	def win(self):
+		pass
+		
+	def add_mark(self, player):
+		pass
+	
+class Player:
+	
+	def __init__(self, name, mark)
+		self.name = name
+		self.mark = mark
+	
